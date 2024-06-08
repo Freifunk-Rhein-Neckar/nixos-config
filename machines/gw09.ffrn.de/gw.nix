9d072c6cb95e22dcf5b6a3479b79781f1d4d8d35
Tom Herbers <mail@tomherbers.de>
2024-06-08 16:41:18 +0200

    machines/gw09.ffrn.de: define remoteLocals accross domains

--- machines/gw09.ffrn.de/gw.nix
+++ machines/gw09.ffrn.de/gw.nix
@@ -175,25 +175,25 @@
   };
 
   modules.freifunk.gateway = {
-    vxlan.local = "2a01:4f8:171:3242:0:8aff:fec9:1ef4";
+    vxlan = {
+      local = "2a01:4f8:171:3242:0:8aff:fec9:1ef4";
+      remoteLocals = [
+        "2a01:4f8:140:4093:0:5eff:fe82:f3e8"    # gw02.ffrn.de
+        "2a01:4f8:171:3242:0:8aff:fec9:1ef7"    # gw03.ffrn.de
+        "2a01:4f8:160:624c:80af:beff:fee3:f047" # gw05.ffrn.de
+        "2a01:4f8:140:4093:0:5eff:fe82:f3e9"    # gw04.ffrn.de
+        "2a01:4f8:160:624c:5054:ff:fed2:43c1"   # gw06.ffrn.de
+        "2a01:4f8:140:4093:0:5eff:fe82:f3eb"    # gw07.ffrn.de
+        "2a01:4f8:160:624c:5054:ff:fe3f:c2ea"   # gw08.ffrn.de
+        # "2a01:4f8:171:3242:0:8aff:fec9:1ef4"    # gw09.ffrn.de
+        "2a01:4f8:160:624c:5054:ff:fed1:ebc5"   # map1.ffrn.de
+        "2a01:4f8:160:624c:5054:ff:fe37:2749"   # resolver1.ffrn.de
+        "2a01:4f8:140:4093:0:c0ff:fea8:7b21"    # resolver2.ffrn.de
+        "2a01:4f8:160:624c:5054:ff:fe62:3d89"   # unifi.ffrn.de
+      ];
+    };
     domains = {
       dom0 = {
-        vxlan = {
-          remoteLocals = [
-            "2a01:4f8:140:4093:0:5eff:fe82:f3e8"    # gw02.ffrn.de
-            "2a01:4f8:171:3242:0:8aff:fec9:1ef7"    # gw03.ffrn.de
-            "2a01:4f8:160:624c:80af:beff:fee3:f047" # gw05.ffrn.de
-            "2a01:4f8:140:4093:0:5eff:fe82:f3e9"    # gw04.ffrn.de
-            "2a01:4f8:160:624c:5054:ff:fed2:43c1"   # gw06.ffrn.de
-            "2a01:4f8:140:4093:0:5eff:fe82:f3eb"    # gw07.ffrn.de
-            "2a01:4f8:160:624c:5054:ff:fe3f:c2ea"   # gw08.ffrn.de
-            # "2a01:4f8:171:3242:0:8aff:fec9:1ef4"    # gw09.ffrn.de
-            "2a01:4f8:160:624c:5054:ff:fed1:ebc5"   # map1.ffrn.de
-            "2a01:4f8:160:624c:5054:ff:fe37:2749"   # resolver1.ffrn.de
-            "2a01:4f8:140:4093:0:c0ff:fea8:7b21"    # resolver2.ffrn.de
-            "2a01:4f8:160:624c:5054:ff:fe62:3d89"   # unifi.ffrn.de
-          ];
-        };
         batmanAdvanced = {
           mac = "6a:ff:94:00:09:02";
         };
@@ -212,7 +212,6 @@
           };
         };
         ipv6 = {
-          # address = "fdeb:52c8:d094:1000::1";
           prefixes = {
             "2a01:4f8:171:fcff::/64" = {
               addresses = [
