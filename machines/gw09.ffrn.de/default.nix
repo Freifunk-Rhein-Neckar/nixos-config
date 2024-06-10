acb7804eb2c7679be588a9f50bb457a633df7193
Tom Herbers <mail@tomherbers.de>
2024-06-10 17:08:04 +0200

    gw09: remove redundant openssh.enable

--- machines/gw09.ffrn.de/default.nix
+++ machines/gw09.ffrn.de/default.nix
@@ -13,7 +13,6 @@
 
   networking.hostName = "gw09";
   networking.domain = "ffrn.de";
-  services.openssh.enable = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
