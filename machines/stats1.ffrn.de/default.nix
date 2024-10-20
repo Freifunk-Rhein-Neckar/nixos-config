b342cf0684aabe84b81b2245e5248f1f3b405c11
Tom Herbers <mail@tomherbers.de>
2024-10-21 00:13:27 +0200

    machines/stats1.ffrn.de: enable zstd compression

--- machines/stats1.ffrn.de/default.nix
+++ machines/stats1.ffrn.de/default.nix
@@ -33,8 +33,10 @@
 
   modules.ffrn.borgbackup.enable = true;
 
-  # Netcup falsely reports their storage as hdd, well at least that is the theory
-  fileSystems."/".options = [ "ssd" ];
+  fileSystems."/".options = [
+    "compress=zstd" # compess data with zstd
+    "ssd"           # Netcup falsely reports their storage as hdd, well at least that is the theory for now
+  ];
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
