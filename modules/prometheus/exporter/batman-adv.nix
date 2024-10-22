41a282e08d5c4e8276df16d997453c0dcfe0a686
Tom Herbers <mail@tomherbers.de>
2024-10-22 15:06:04 +0200

    fixup! batman-adv-exporter WIP

--- modules/prometheus/exporter/batman-adv.nix
+++ modules/prometheus/exporter/batman-adv.nix
@@ -10,14 +10,15 @@
     ];
   };
 
-  systemd.services.prometheus-batadv-txtexport = {
-    description = "Run prometheus exporter batadv-txtexport script";
+  systemd.services.prometheus-batadv-textexport = {
+    description = "Run prometheus exporter batadv-textexport script";
     serviceConfig = {
       Type = "oneshot";
       ExecStartPre = "${pkgs.busybox}/bin/mkdir -p /var/lib/prometheus-node-exporter";
       ExecStart = "${pkgs.writeScript "prometheus-batadv-txtexport" ''
         #!/bin/sh
         BATCTL=${pkgs.batctl}/bin/batctl
+        OUTFILE=/var/lib/prometheus-node-exporter/batadv.prom
 
         for batdev in /sys/class/net/bat-dom*; do
           test -d $batdev || exit 0
@@ -27,15 +28,14 @@
               gsub(":", "");
               print "batman_" $1 "{batdev=\"" batdev "\"} " $2
             }
-          '
+          ' > $OUTFILE
 
-          echo "batman_originator_count{batdev=\"$batdev\",selected=\"false\"}" $($BATCTL meshif $batdev o | ${pkgs.busybox}/bin/egrep '^   ' | ${pkgs.busybox}/bin/wc -l)
-          echo "batman_originator_count{batdev=\"$batdev\",selected=\"true\"}" $($BATCTL meshif $batdev o | ${pkgs.busybox}/bin/egrep '^ \*' | ${pkgs.busybox}/bin/wc -l)
-          echo "batman_tg_count{batdev=\"$batdev\",type=\"multicast\"}" $(($($BATCTL meshif $batdev tg -m | ${pkgs.busybox}/bin/wc -l) - 2))
-          echo "batman_tg_count{batdev=\"$batdev\",type=\"unicast\"}" $(($($BATCTL meshif $batdev tg -u | ${pkgs.busybox}/bin/wc -l) - 2))
+          echo "batman_originator_count{batdev=\"$batdev\",selected=\"false\"}" $($BATCTL meshif $batdev o | ${pkgs.busybox}/bin/egrep '^   ' | ${pkgs.busybox}/bin/wc -l) >> $OUTFILE
+          echo "batman_originator_count{batdev=\"$batdev\",selected=\"true\"}" $($BATCTL meshif $batdev o | ${pkgs.busybox}/bin/egrep '^ \*' | ${pkgs.busybox}/bin/wc -l) >> $OUTFILE
+          echo "batman_tg_count{batdev=\"$batdev\",type=\"multicast\"}" $(($($BATCTL meshif $batdev tg -m | ${pkgs.busybox}/bin/wc -l) - 2)) >> $OUTFILE
+          echo "batman_tg_count{batdev=\"$batdev\",type=\"unicast\"}" $(($($BATCTL meshif $batdev tg -u | ${pkgs.busybox}/bin/wc -l) - 2)) >> $OUTFILE
         done
       ''}";
-      StandardOutput = "file:/var/lib/prometheus-node-exporter/batadv.prom";
     };
   };
 
@@ -43,8 +43,7 @@
     description = "Run prometheus exporter batadv-txtexport script periodically";
     wantedBy = [ "timers.target" ];
     timerConfig = {
-      OnCalendar = "Minutely"; # Adjust as needed
-      # Unit = "prometheus-batadv-txtexport.service";
+      OnCalendar = "Minutely";
     };
   };
 }
