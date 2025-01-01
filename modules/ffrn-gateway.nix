92cb09cb254b1c54a27136957dda94813270d58f
Tom Herbers <mail@tomherbers.de>
2025-01-01 03:07:43 +0100

    niv: update nixos-freifunk
    View changes: https://github.com/nix-freifunk/nixos-freifunk/compare/5067fb665c26353ab69824185c2cffcb335f0cd2...5a7439c91382d921afa5462d790486fae471dcab
    
    Changelog:
    
    5a7439c gateway: don't try to configure forwarding between domains with single domain
    036622b gateway: fix mss clmaping interface lists
    bc7345e gateway: switch from outInterface (str) to outInterfaces (listOf str)
    49019e5 gateway: fix typo in firewall rule
    8765f62 gateway: allow forwarding between domains
    7d25708 gateway: fix typo (domain instead of prefix)
    b3661b5 bird: add extraFilters option

--- modules/ffrn-gateway.nix
+++ modules/ffrn-gateway.nix
@@ -9,6 +9,8 @@ let
 
   padString = (n: if n < 10 then "0" + toString n else toString n);
 
+  mainif = "${config.systemd.network.networks."10-mainif".matchConfig.Name}";
+
 in
 {
   options.modules.ffrn-gateway = {
@@ -27,7 +29,7 @@ in
   config = mkIf cfg.enable {
     networking.nftables.tables.postrouting.content = ''
       chain postrouting_extra {
-        ip saddr 10.142.0.0/16 oifname "${config.modules.freifunk.gateway.outInterface}" ip daddr != { 10.0.0.0/8, 172.16.0.0/12, 192.0.2.0/24, 192.168.0.0/16, 198.51.100.0/24, 203.0.113.0/24 } counter snat to ${cfg.publicIPv4};
+        ip saddr 10.142.0.0/16 oifname "${mainif}" ip daddr != { 10.0.0.0/8, 172.16.0.0/12, 192.0.2.0/24, 192.168.0.0/16, 198.51.100.0/24, 203.0.113.0/24 } counter snat to ${cfg.publicIPv4};
       }
     '';
 
@@ -88,6 +90,7 @@ in
     };
 
     modules.freifunk.gateway = {
+      outInterfaces = [ "${mainif}" ];
       vxlan = {
         local = cfg.publicIPv6;
         remoteLocals = lib.filter (ip: ip != config.modules.freifunk.gateway.vxlan.local) [
