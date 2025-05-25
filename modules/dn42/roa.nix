e892967a623292d244bbfaaa3d610cab8188d391
Tom Herbers <mail@tomherbers.de>
2025-05-25 23:16:44 +0200

    npins: update nixpkgs to nixos-25.05

--- modules/dn42/roa.nix
+++ modules/dn42/roa.nix
@@ -10,19 +10,19 @@
     extraConfig = ''
       protocol static dn42_roa4 {
         roa4 { table dn42_roa_v4; };
-        include "dn42_roa_bird2_4.conf";
+        include "dn42_roa_bird_4.conf";
       };
 
       protocol static dn42_roa6 {
         roa6 { table dn42_roa_v6; };
-        include "dn42_roa_bird2_6.conf";
+        include "dn42_roa_bird_6.conf";
       };
     '';
   };
 
-  services.bird2.preCheckConfig = ''
-    echo "" > dn42_roa_bird2_4.conf;
-    echo "" > dn42_roa_bird2_6.conf;
+  services.bird.preCheckConfig = ''
+    echo "" > dn42_roa_bird_4.conf;
+    echo "" > dn42_roa_bird_6.conf;
   '';
 
   systemd.services.dn42-roa = {
@@ -30,10 +30,10 @@
     serviceConfig = {
       Type = "oneshot";
       ExecStart = [
-        "${pkgs.curl}/bin/curl -sfSLR -o /etc/bird/dn42_roa_bird2_4.conf -z /etc/bird/dn42_roa_bird2_4.conf https://dn42.burble.com/roa/dn42_roa_bird2_4.conf"
-        "${pkgs.curl}/bin/curl -sfSLR -o /etc/bird/dn42_roa_bird2_6.conf -z /etc/bird/dn42_roa_bird2_6.conf https://dn42.burble.com/roa/dn42_roa_bird2_6.conf"
+        "${pkgs.curl}/bin/curl -sfSLR -o /etc/bird/dn42_roa_bird_4.conf -z /etc/bird/dn42_roa_bird_4.conf https://dn42.burble.com/roa/dn42_roa_bird2_4.conf"
+        "${pkgs.curl}/bin/curl -sfSLR -o /etc/bird/dn42_roa_bird_6.conf -z /etc/bird/dn42_roa_bird_6.conf https://dn42.burble.com/roa/dn42_roa_bird2_6.conf"
       ];
-      ExecStartPost = "${pkgs.systemd}/bin/systemctl reload bird2.service";
+      ExecStartPost = "${pkgs.systemd}/bin/systemctl reload bird.service";
     };
   };
 
