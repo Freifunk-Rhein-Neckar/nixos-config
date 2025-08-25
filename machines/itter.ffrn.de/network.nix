d5af56be9843c1f2ebd5c00121898359019989af
Tom Herbers <mail@tomherbers.de>
2025-08-25 22:08:54 +0200

    machines/itter.ffrn.de/network: route net to gw-test02

--- machines/itter.ffrn.de/network.nix
+++ machines/itter.ffrn.de/network.nix
@@ -408,7 +408,7 @@
           route ::/0 via fe80::1%${config.systemd.network.links."10-mainif".linkConfig.Name};
           route 2a01:4f8:140:7700::/56 unreachable;
           #route 2a01:4f8:160:9700::/64 via "lo";
-          #route 2a01:4f8:160:97c0::/60 via 2a01:4f8:140:4093:5054:ff:fe02:e2a6; # gw-test02
+          route 2a01:4f8:160:97c0::/60 via 2a01:4f8:140:4093:5054:ff:fe02:e2a6; # gw-test02
 
           route 2a01:4f8:160:624c:5054:ff:fe62:3d89/128 via 2a01:4f8:140:4093:5054:ff:fe62:3d89;
 
