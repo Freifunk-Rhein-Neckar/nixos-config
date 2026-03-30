6a0ac6a00a6edd5f72fedef24a44e6ea7ae724c9
Tom Herbers <mail@tomherbers.de>
2026-03-30 02:18:40 +0200

    roles/gw/ffrn-hetzner-vm-elsenz: anounce itter v6 range
    This is done so that the elsenz range is no longer used in
    anticipation of the cancellation of elsenz.

--- roles/gw/ffrn-hetzner-vm-elsenz.nix
+++ roles/gw/ffrn-hetzner-vm-elsenz.nix
@@ -6,7 +6,8 @@
   ];
 
   modules.freifunk.gateway.domains = {
-    dom0.ipv6.prefixes."2a01:4f8:171:fcff::/64".announce = true;
+    dom0.ipv6.prefixes."2a01:4f8:171:fcff::/64".announce = false;
+    dom0.ipv6.prefixes."2a01:4f8:140:7700::/64".announce = true; # annouce itter range on elsenz for now
   };
 
 }
