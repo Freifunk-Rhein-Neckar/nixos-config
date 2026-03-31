6e63dd637bd638f629f22658849cb61969c6ba7b
Tom Herbers <mail@tomherbers.de>
2026-03-31 04:33:36 +0200

    machines/[weschnitzs|itter]: disable libvirtd
    We're only using incus now.

--- machines/weschnitz.ffrn.de/default.nix
+++ machines/weschnitz.ffrn.de/default.nix
@@ -58,8 +58,6 @@
     ];
   };
 
-  virtualisation.libvirtd.enable = true;
-
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
