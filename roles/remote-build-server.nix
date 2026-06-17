cd65cf3262a2c283fb70612e5ff0c18153cd4807
Tom Herbers <mail@tomherbers.de>
2026-06-18 01:02:46 +0200

    roles/remote-build-server: update some keys

--- roles/remote-build-server.nix
+++ roles/remote-build-server.nix
@@ -5,8 +5,8 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKdnwVGpMaBv5Bx2XuIvuBI+b4HNaPYcuPoGSzZi/Z5R ffrn@tomh v1"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILp4HgDDRQYOp1xXPTUkqv83dZw+DGIj5jZdBzR2u57Y tom@tomh v6"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMCK8LTP9ElXbQqCsvAMSd0G9l6a/Oneh+k3ocPsSa4H root@tom-laptop2"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDhYQY4ydpa0w6+1+7O2cXEDBdlYMgYeLT9UCSO5t74/ root@tom-zeus"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP5g4W9Sq6zk1HOz70VWHhrYTcIDhL5mauv6uBXzVL9t root@tom-laptop3"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJWo8RHWi3Ulv38FLja4GJDxgGToxeKUoUl0KPIS0Bu7 root@zeus"
     ];
   };
 
